3947642879a7a3888f6a47aae4e96b699763deeb
AdrianFont <adrian.font.esteban@gmail.com>
2026-03-15 20:43:20 +0100

    Todos los rowmapper listos

---
+++ src/main/java/rowmapper/AsistenciaFormacionRowMapper.java
@@ -0,0 +1,27 @@
+package rowmapper;
+
+import model.AsistenciaFormacion;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AsistenciaFormacionRowMapper implements RowMapper<AsistenciaFormacion> {
+
+    @Override
+    public AsistenciaFormacion mapRow(ResultSet rs, int rowNum) throws SQLException {
+        AsistenciaFormacion asistencia = new AsistenciaFormacion();
+        asistencia.setIdAsistencia(rs.getInt("id_asistencia"));
+        asistencia.setIdUsuari(rs.getInt("id_usuari"));
+        asistencia.setIdActividad(rs.getInt("id_actividad"));
+        asistencia.setFechaAsistencia(rs.getDate("fecha_asistencia") != null ?
+                rs.getDate("fecha_asistencia").toLocalDate() : null);
+        asistencia.setHoraEntrada(rs.getTime("hora_entrada") != null ?
+                rs.getTime("hora_entrada").toLocalTime() : null);
+        asistencia.setHoraSalida(rs.getTime("hora_salida") != null ?
+                rs.getTime("hora_salida").toLocalTime() : null);
+        asistencia.setCertificadoEmitido(rs.getBoolean("certificado_emitido"));
+        asistencia.setObservaciones(rs.getString("observaciones"));
+        return asistencia;
+    }
+}
