f851d7161b5738863cc4a9e2628b5d95d52406be
febbyguillero <al427156@uji.es>
2026-05-28 14:39:28 +0200

    mis contratos usuari OVI

--- src/main/java/es/uji/ei1027/sgovid/controller/SolicitudController.java
+++ src/main/java/es/uji/ei1027/sgovid/controller/SolicitudController.java
@@ -1,8 +1,10 @@
 package es.uji.ei1027.sgovid.controller;
 
 import es.uji.ei1027.sgovid.dao.APRequestDao;
+import es.uji.ei1027.sgovid.dao.RegistroContratoDao;
 import es.uji.ei1027.sgovid.dao.UsuarioOVIDao;
 import es.uji.ei1027.sgovid.model.APRequest;
+import es.uji.ei1027.sgovid.model.RegistroContrato;
 import es.uji.ei1027.sgovid.model.UsuarioOVI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -22,6 +25,10 @@ public class SolicitudController {
     @Autowired
     private UsuarioOVIDao usuarioDao;
 
+    @Autowired
+    private RegistroContratoDao contratoDao;
+
+//nuevasolicitud
     @GetMapping("/nueva")
     public String mostrarFormulario(Model model) {
         model.addAttribute("solicitud", new APRequest());
@@ -69,6 +76,8 @@ public class SolicitudController {
         return "redirect:/solicitudes/mis-solicitudes";
     }
 
+    //solicitudes
+
     @GetMapping("/mis-solicitudes")
     public String misSolicitudes(Model model, jakarta.servlet.http.HttpSession session) {
         Object usuariId = session.getAttribute("usuariId");
@@ -82,6 +91,8 @@ public class SolicitudController {
         return "solicitudes/mis-solicitudes";
     }
 
+    //miperfil
+
     @GetMapping("/mi-perfil")
     public String miPerfil(Model model, jakarta.servlet.http.HttpSession session) {
         Object usuariId = session.getAttribute("usuariId");
@@ -89,5 +100,21 @@ public class SolicitudController {
         UsuarioOVI usuario = usuarioDao.getUsuario(Integer.parseInt(usuariId.toString()));
         model.addAttribute("usuario", usuario);
         return "solicitudes/mi-perfil";
+    }
+
+    //contrartos
+
+    @GetMapping("/mis-contratos")
+    public String misContratos(Model model, jakarta.servlet.http.HttpSession session) {
+        Object usuariId = session.getAttribute("usuariId");
+        if (usuariId == null) return "redirect:/login";
+        List<APRequest> solicitudes = apRequestDao.getRequestsByUsuari(usuariId.toString());
+        List<RegistroContrato> contratos = new ArrayList<>();
+        for (APRequest sol : solicitudes) {
+            RegistroContrato c = contratoDao.getContratoBySolicitud(sol.getIdRequest());
+            if (c != null) contratos.add(c);
+        }
+        model.addAttribute("contratos", contratos);
+        return "solicitudes/mis-contratos";
     }
 }
